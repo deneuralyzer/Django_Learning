584978beeea75630f57351e37ad937ab1d33aa31
pocket_money <deneuralyzer@gmail.com>
2021-03-17 10:05:35 +0800

    Templates, Class, Test

--- page/views.py
+++ page/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def homePageView(request):
-    return HttpResponse('HellowWorld!')
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
